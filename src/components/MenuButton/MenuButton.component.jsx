804cc53fb15294ad79da083d1ddccf6b17e7ba90
Florian Ipate <florian.ipate@gmail.com>
2022-09-30 22:17:29 +0100

    added fontwasome

---
+++ src/components/MenuButton/MenuButton.component.jsx
@@ -0,0 +1,8 @@
+const MenuButton = () =>{
+    return(
+        <>
+        
+        </>
+    )
+}
+export default MenuButton;
--- src/components/TopNavBar/TopNavBar.component.jsx
+++ src/components/TopNavBar/TopNavBar.component.jsx
@@ -1,11 +1,17 @@
 //import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import Logo from '../Logo/Logo,comonent';
+//import MenuButton from '../MenuButton/MenuButton.component';
 
 const TopNavBar = () =>{
+    const [click, setClick] = useState(false);
+    const handleClick =() => setClick(!click);
     return(
         <>
         <Logo />
-
+        <div onClick={handleClick}>
+            <i className= {click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
         </>
     )
 }
