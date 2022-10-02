e00168c45dd4bcc6825b36b4dfc330733d8500ba
Florian Ipate <florian.ipate@gmail.com>
2022-10-02 23:05:25 +0100

    added link

--- src/components/TopNavBar/TopNavBar.component.jsx
+++ src/components/TopNavBar/TopNavBar.component.jsx
@@ -1,4 +1,4 @@
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import Logo from '../Logo/Logo,comonent';
 //import MenuButton from '../MenuButton/MenuButton.component';
@@ -12,6 +12,13 @@ const TopNavBar = () =>{
         <div onClick={handleClick}>
             <i className= {click ? 'fas fa-times' : 'fas fa-bars'} />
         </div>
+        <ul className={click ? 'nam-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+                <link to = '/' className='nav-links' onClick={closeMobileMenu}>
+                    Home
+                </link>
+            </li>
+        </ul>
         </>
     )
 }
